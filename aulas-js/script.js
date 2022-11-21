3b0c69b4cd1de2f72a8e12cd7250fe4e22bcdb68
Bernardo Duarte de Miranda <112958256+bernardodm@users.noreply.github.com>
2022-11-21 11:27:23 -0300

    Testes JS

---
+++ aulas-js/script.js
@@ -0,0 +1,16 @@
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>";
+
+console.log('Oi, isso é um console.log')
+
+//Esse é um comentário
+/*
+Esse tabém é um comentário
+*/
+//declaração de variaveis
+let x=1;
+{
+    let x=2;
+    document.write(x)
+}
+
+document.getElementById("texto").innerHTML=(x);
