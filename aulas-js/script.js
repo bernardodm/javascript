f4646429b88b2d058f2eabc92e7e202b0a6144ea
Bernardo Duarte de Miranda <112958256+bernardodm@users.noreply.github.com>
2022-11-28 10:51:22 -0300

    Update script.js

--- aulas-js/script.js
+++ aulas-js/script.js
@@ -26,7 +26,7 @@ var y = realParaDolar(10, 5.08);
 document.getElementById("texto").innerHTML = total;
 */
 
-
+/*
 function clicou (){
     document.body.style.backgroundColor = "blue";
 }
@@ -63,8 +63,50 @@ const lista = [
     "leite"
 ];
 
-alert (lista[2])
+const lista1 = [
+    "batata",
+    "beterraba",
+    "repolo"
+];
+
+const listao = lista.concat (lista1); //Junta duas matrizes
+
+const comida = lista.slice (0,2); //divide duas matrizes da casa onde inicia até a casa onde para, nesse caso é para mostrar só a acasa 0 e 1
+
+lista.sort (); //Usar de forma alfabética
+
+const nume = [
+    40,
+    10,
+    20,
+    50,
+    30 
+]
+
+alert (nume.sort (function (a,b) {return a-b})); //colocar numeros em ordem crescente
+
+
+
+
+
+function maiornume(array){ //Colocar o maior número
+    return Math.max.apply(null, array);//
+}//
+//
+document.getElementById("texto").innerHTML = maiornume(nume);//
+
+
+
+
+
+
+
+lista1.pop(); //apagar o último da lista
+
+
 
 //Para mostrar o total de itens na matriz, basta utilizar o lista.length
 //alert (lista.length);
 //Para acrescentar um item na matriz, basta utilizar o lista.push("item"), assim, entrará mais um na lista
+*/
+
